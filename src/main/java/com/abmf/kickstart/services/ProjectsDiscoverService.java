1509eb5574bf1f5404887c9b0b6e0dc70491447b
ABuarque <abuarquemf@gmail.com>
2018-04-28 19:07:26 -0300

    added new service

---
+++ src/main/java/com/abmf/kickstart/services/ProjectsDiscoverService.java
@@ -0,0 +1,24 @@
+package main.java.com.abmf.kickstart.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import main.java.com.abmf.kickstart.models.categories.Category;
+import main.java.com.abmf.kickstart.models.project.Project;
+import main.java.com.abmf.kickstart.view.KickStarterDeployer;
+
+public class ProjectsDiscoverService implements Service {
+	
+	public List<Project> getAllProjects() {
+		return KickStarterDeployer.kickStarterInstance.getProjects();
+	}
+	
+	public List<Project> getProjectsByCategory(Category category) {
+		List<Project> projects = new ArrayList<>();
+		for(Project project : KickStarterDeployer.kickStarterInstance.getProjects())
+			if(project.getCategory().equals(category))
+				projects.add(project);
+		return projects;
+	}
+
+}
