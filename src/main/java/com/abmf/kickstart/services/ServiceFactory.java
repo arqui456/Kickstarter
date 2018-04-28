7a8bbfc680323ec3c53bda550d86141cea2e5869
ABuarque <abuarquemf@gmail.com>
2018-04-28 16:57:58 -0300

    added singleton

--- src/main/java/com/abmf/kickstart/services/ServiceFactory.java
+++ src/main/java/com/abmf/kickstart/services/ServiceFactory.java
@@ -2,6 +2,16 @@ package main.java.com.abmf.kickstart.services;
 
 public class ServiceFactory {
 	
+	private static ServiceFactory INSTANCE = null;
+	
+	private ServiceFactory() {}
+	
+	public static ServiceFactory getInstance() {
+		if(INSTANCE == null)
+			INSTANCE = new ServiceFactory();
+		return INSTANCE;
+	}
+	
 	public Service getService(ServiceType serviceType) {
 		if(serviceType == ServiceType.AUTH)
 			return new AuthService();
